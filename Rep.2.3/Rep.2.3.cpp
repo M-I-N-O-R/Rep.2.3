ca6486fe93b06523100e2d8e59fae28d42f5f2b3
Олександр <goncars451@gmail.com>
2024-10-04 22:27:52 +0300

    Добавьте файлы проекта.

---
+++ Rep.2.3/Rep.2.3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(0, ".1251");
+    int n;
+    cout << "Введiть кiлькiсть рядкiв для трикутника: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
